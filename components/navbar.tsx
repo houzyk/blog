bc03df8a9810e19b74fa6886abaa2bfd3882ab3a
houzairkoussa <houzairmk@icloud.com>
2022-01-06 19:41:48 +0400

    ADD navbar and night toggle components with css

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import styles from "../styles/components/Navbar.module.css";
+
+const Navbar: FunctionComponent = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ components/night_toggle.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import styles from "../styles/components/NightToggle.module.css";
+
+const NightToggle: FunctionComponent = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default NightToggle;
