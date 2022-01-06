69ad9f1b8820d13e88f2666a624a216de613a230
houzairkoussa <houzairmk@icloud.com>
2022-01-06 19:33:24 +0400

    ADD article 2 page and css

---
+++ pages/keepitdry.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Article.module.css";
+
+const KeepItDry: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Keep It Dry</title>
+      </Head>
+    </>
+  );
+}
+
+export default KeepItDry;
