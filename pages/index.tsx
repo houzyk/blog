fcabf1eb1b7efc38fdfacce255142920aff1c205
houzairkoussa <houzairmk@icloud.com>
2022-01-06 19:32:39 +0400

    ADD article 2 page and css

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>The Philosophical Code</title>
       </Head>
-    </div>
+    </>
   )
 }
 
