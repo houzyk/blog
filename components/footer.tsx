d0283261111726a492878913574302d4a6805590
houzairkoussa <houzairmk@icloud.com>
2022-01-07 18:16:20 +0400

    ADD footer component + CSS

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import styles from "../styles/components/Footer.module.css";
+
+const Footer: FunctionComponent = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Footer;
