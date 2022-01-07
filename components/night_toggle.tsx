cd75963e0485623926c01e8276e2d740e003fa76
houzairkoussa <houzairmk@icloud.com>
2022-01-07 23:47:24 +0400

    ADD toggle css todo- animate translation

--- components/night_toggle.tsx
+++ components/night_toggle.tsx
@@ -1,23 +1,25 @@
 // react
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 // css
 import styles from "../styles/components/NightToggle.module.css";
 
 const NightToggle: FunctionComponent = () => {
+  const [light, setLight] = useState(true)
 
   // changes view mode upon click
   const handleViewMode = ():void => {
+    setLight(!light)
     const body: HTMLBodyElement | null = document.querySelector('body')
     if (body) body.classList.toggle("night")
   }
 
   return (
     <div
-      className= {styles.toggleBox}
+      className= {light ? styles.boxLight : styles.boxNight}
     >
       <div
-        className={styles.button}
+        className={light ? styles.buttonLight : styles.buttonNight}
         onClick={():void => handleViewMode()}
       />
     </div>
