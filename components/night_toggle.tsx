0c09207d2214bbf1846e87f644cfe377ba21d0e4
houzairkoussa <houzairmk@icloud.com>
2022-01-07 23:12:06 +0400

    ADD night mode toggler functionality

--- components/night_toggle.tsx
+++ components/night_toggle.tsx
@@ -1,10 +1,23 @@
-import React, { FunctionComponent } from "react";
+// react
+import React, { FunctionComponent, useState} from "react";
+
+// css
 import styles from "../styles/components/NightToggle.module.css";
 
 const NightToggle: FunctionComponent = () => {
+
+  // changes view mode upon click
+  const handleViewMode = ():void => {
+    const body: HTMLBodyElement | null = document.querySelector('body')
+    if (body) body.classList.toggle("night")
+  }
+
   return (
-    <>
-    </>
+    <div
+    className="night-toggle"
+    onClick={():void => handleViewMode()}>
+      {/* todo */}
+    </div>
   );
 }
 
