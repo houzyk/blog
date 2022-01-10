b85fe616a73398b800cd005a89124e338d8f600e
houzairkoussa <houzairmk@icloud.com>
2022-01-10 20:28:23 +0400

    ADD handler for digit date with 0

--- components/today.tsx
+++ components/today.tsx
@@ -6,9 +6,11 @@ import styles from "../styles/components/Today.module.css"
 
 const Today: FunctionComponent = () => {
   const dateToday: Date = new Date()
+  let month: string = `0${dateToday.getMonth() + 1}`
+  if (dateToday.getMonth() > 9) month = 0 + month
   return (
     <p>
-      {dateToday.getDate()}/{dateToday.getMonth() + 1}/{dateToday.getFullYear()}
+      {dateToday.getDate()}/{month}/{dateToday.getFullYear()}
     </p>
   );
 }
