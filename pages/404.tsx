4bf8c6d02154302723494110c312c7d9942a7a27
houzairkoussa <houzairmk@icloud.com>
2022-01-10 20:10:50 +0400

    ADD CSS path to error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,9 @@
 // react
 import React, { FunctionComponent } from "react"
 
+// css
+import styles from "../styles/pages/Error.module.css";
+
 const fourHundred: FunctionComponent = () => {
   return (
     <>
